9958918f81aec12b8bec2a85654619c61e211b46
TDL2627 <the.dark.lord.2627@gmail.com>
2022-04-09 13:06:37 +0200

    hero update

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -3,12 +3,12 @@ import { Hero } from './hero';
 export const HEROES: Hero[] = [
   { id: 1, name: 'Goku' },
   { id: 2, name: 'Saitama' },
-  { id: 3, name: 'Bombasto' },
-  { id: 4, name: 'Celeritas' },
-  { id: 5, name: 'Magneta' },
-  { id: 6, name: 'RubberMan' },
-  { id: 7, name: 'Dynama' },
-  { id: 8, name: 'Dr IQ' },
-  { id: 9, name: 'Magma' },
-  { id: 10, name: 'Tornado' }
+  { id: 3, name: 'Ben 10' },
+  { id: 4, name: 'Ash' },
+  { id: 5, name: 'Midoriya' },
+  { id: 6, name: 'Eren' },
+  { id: 7, name: 'Steven Universe' },
+  { id: 8, name: 'Yugi oh' },
+  { id: 9, name: 'Baki' },
+  { id: 10, name: 'Super Man' }
 ];
